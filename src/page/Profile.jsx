57927c53e1d879e6792e097e66ed0f0cb0822aef
Sahil <sahilmahimkar3@gmail.com>
2023-03-12 05:55:56 +0530

    added achievements

--- src/page/Profile.jsx
+++ src/page/Profile.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Avatar, Stack, Group, Box, TextInput } from "@mantine/core";
+import { Avatar, Stack, Group, Box, TextInput, Badge } from "@mantine/core";
 import { HeadFootLayout } from "../components/Layout/Layout";
 import useMainStore from "../store/mainStore";
 
 const Profile = () => {
   const userDetails = useMainStore((state) => state.userDetails);
+  const avatar = (
+    <Avatar alt="Avatar for badge" size={24} mr={5} src="./avatar.png" />
+  );
 
   return (
     <HeadFootLayout>
@@ -22,6 +25,20 @@ const Profile = () => {
         <Box maw={300}>
           <TextInput label="Email" value={userDetails.email} disabled />
         </Box>
+        <Group align="flex-start" grow>
+          <Badge pl={0} size="lg" color="teal" radius="xl" leftSection={avatar}>
+            10 Hrs Of Study
+          </Badge>
+          <Badge pl={0} size="lg" color="teal" radius="xl" leftSection={avatar}>
+            Completed 10 Pomodoros
+          </Badge>
+          <Badge pl={0} size="lg" color="teal" radius="xl" leftSection={avatar}>
+            Web Mastery
+          </Badge>
+          <Badge pl={0} size="lg" color="teal" radius="xl" leftSection={avatar}>
+            Listen to Music
+          </Badge>
+        </Group>
       </Stack>
     </HeadFootLayout>
   );
